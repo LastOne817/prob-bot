faacedd97a91dc09ee8db6d86b7e8010034fdceb
LastOne817 <lastone817@naver.com>
2016-05-03 16:55:27 +0900

    BugFix

--- bot.py
+++ bot.py
@@ -4,27 +4,6 @@ from connector.ircmessage import IRCMessage
 from queue import Queue
 
 
-def performAction(packet):
-    if packet['type'] == 'msg':
-        msg = packet['content']
-        for channel in self.channel_list:
-            self.irc.sendmsg(channel, msg)
-
-    elif packet['type'] == 'irc':
-        message = packet['content']
-        print(message)
-        if message.msgType == 'INVITE':
-            self.irc.joinchan(message.channel)
-
-        elif message.msgType == 'MODE':
-            if message.msg == '+o ' + self.irc.botnick:
-                self.irc.sendmsg(message.channel, '감사합니다 :)')
-
-        elif message.msgType == 'KICK':
-            pass
-
-        elif message.msgType == 'PRIVMSG':
-            pass
 
 
 class Bot():
@@ -40,7 +19,26 @@ class Bot():
     def run(self):
         while True:
             packet = self.msgQueue.get()
-            performAction(packet)
+            if packet['type'] == 'msg':
+                msg = packet['content']
+                for channel in self.channel_list:
+                    self.irc.sendmsg(channel, msg)
+
+            elif packet['type'] == 'irc':
+                message = packet['content']
+                print(message)
+                if message.msgType == 'INVITE':
+                    self.irc.joinchan(message.channel)
+
+                elif message.msgType == 'MODE':
+                    if message.msg == '+o ' + self.irc.botnick:
+                        self.irc.sendmsg(message.channel, '감사합니다 :)')
+
+                elif message.msgType == 'KICK':
+                    pass
+
+                elif message.msgType == 'PRIVMSG':
+                    pass
 
 
 if __name__ == '__main__':
